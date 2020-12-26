bb1e66912f15c671572a5649ea01e820ea7b508a
juunini <juuni.ni.i@gmail.com>
2020-12-27 00:41:44 +0900

    Add test shell script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+go test --coverprofile=coverage.out ./...
+
+covered=0
+total=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    IFS=' ' read -r -a array <<< "$line"
+    total=$(($total+${array[1]}))
+    if [ "${array[2]}" = "1" ]; then
+        covered=$(($covered+${array[1]}))
+    fi 
+done < "coverage.out"
+echo "global coverage: "
+echo $(awk "BEGIN { pc=100*${covered}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+
+rm coverage.out
