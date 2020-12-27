2a748da5c1fa05201b7142dc555dee0e7e7b7280
juunini <juuni.ni.i@gmail.com>
2020-12-27 10:22:31 +0900

    Fix test result message

--- test.sh
+++ test.sh
@@ -11,7 +11,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
         covered=$(($covered+${array[1]}))
     fi 
 done < "coverage.out"
-echo "global coverage: "
-echo $(awk "BEGIN { pc=100*${covered}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+
+echo ""
+echo "global coverage:" $(awk "BEGIN { pc=100*${covered}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")"% of statements"
 
 rm coverage.out
