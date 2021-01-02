866e1d6739773ad50d57a10f5f71099784caa04e
juunini <juuni.ni.i@gmail.com>
2021-01-02 23:41:52 +0900

    Add ProductSize type

--- crawlers/crawler.go
+++ crawlers/crawler.go
@@ -5,3 +5,9 @@ type ProductImage struct {
 	Thumbnail string
 	Src       string
 }
+
+// ProductSize type of product size name and in stock
+type ProductSize struct {
+	Name    string
+	InStock bool
+}
